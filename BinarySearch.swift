c3242196320020c6b6c48c8f210e71bd55b92853
Ubuntu <ubuntu@ip-172-31-92-172.ec2.internal>
2021-12-04 18:48:43 +0000

    added comments

--- BinarySearch.swift
+++ BinarySearch.swift
@@ -1,12 +1,13 @@
 //
-//  Factorial.swift
+//  Binary.swift
 //
 //  Created by Ryan Chung
-//  Created on 2021-12-01
+//  Created on 2021-12-03
 //  Version 1.0
 //  Copyright (c) 2021 Ryan Chung. All rights reserved.
 //
-//  This program calculates the factorial of a user-inputted integer.
+//  This program finds a user-inputted number inside of an array using
+//  recursion and binary search.
 //
 
 import Foundation
@@ -14,6 +15,7 @@ import Foundation
 let min = 0
 let max = 999
 
+// Binary search using recursion
 func binarySearch(numberArray: [Int], userNumber: Int,
                   lowIndex: Int, highIndex: Int) -> Int {
 
@@ -44,6 +46,7 @@ func binarySearch(numberArray: [Int], userNumber: Int,
     return returnValue
 }
 
+// Generates an array populated with 250 random numbers
 func generateRandomArray() -> [Int] {
     let arraySize = 250
 
@@ -53,32 +56,42 @@ func generateRandomArray() -> [Int] {
     return numberArray
 }
 
+// Reusable error message
 func errorMessage() {
     print("ERROR: Invalid Input")
     print("\nDone.")
 }
 
+// Creating the random array
 let numberArray = generateRandomArray()
 
+// Prints a formatted version of the array
 print("\nSorted list of numbers:\n")
 for element in numberArray {
     let padded = String(format: "%03d", element)
     print("\(padded), ", terminator: "")
 }
 
+// User prompt
 print("\n\nWhat number are you searching for in the array"
     + "(integer between 0 and 999): ", terminator: "")
 
+// Gathers and makes sure the user inputted something
 guard let searchNumberString = readLine(), !searchNumberString.isEmpty else {
     errorMessage()
     exit(001)
 }
 
+// Checks if the inputted string can be parsed to an Int
 if let searchNumber = Int(searchNumberString) {
+
+    // Makes sure the user inputted something in between the bounds
     if searchNumber < min || searchNumber > max {
         errorMessage()
         exit(001)
     } else {
+
+        // Gets the index of the number within the array
         let searchResult = binarySearch(
             numberArray: numberArray, userNumber: searchNumber,
             lowIndex: 0, highIndex: numberArray.count - 1)
